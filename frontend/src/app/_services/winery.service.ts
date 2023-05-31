61d70aea7df9549284a277313108962df6649db0
vafdaf12 <vafdaf11@gmail.com>
2023-05-31 16:04:08 +0200

    Implement winery service using Mock data from Faker.js

--- frontend/src/app/_services/winery.service.ts
+++ frontend/src/app/_services/winery.service.ts
@@ -1,4 +1,21 @@
 import { Injectable } from '@angular/core';
+import { Observable, of } from 'rxjs';
+import { SortBy, SortOrder, Winery } from '../_types';
+import { faker } from "@faker-js/faker";
+
+
+function generateWinery(): Winery {
+  return {
+    id: faker.number.int(),
+    name: faker.company.name(),
+    description: faker.lorem.paragraph(20),
+    established: faker.date.past().getFullYear(),
+    location: faker.location.streetAddress(),
+    region: faker.location.county(),
+    country: faker.location.country(),
+    website: faker.internet.url()
+  }
+}
 
 @Injectable({
   providedIn: 'root'
@@ -6,4 +23,17 @@ import { Injectable } from '@angular/core';
 export class WineryService {
 
   constructor() { }
+
+  getAll(sortBy?: SortBy<Winery>): Observable<Winery[]> {
+    let arr = faker.helpers.multiple(generateWinery, {
+      count: 10
+    });
+    if(sortBy) arr = arr.sort((a, b) => {
+      if(a[sortBy.key] < b[sortBy.key]) return -1;
+      if(a[sortBy.key] > b[sortBy.key]) return 1;
+      return 0;
+    });
+
+    return of(arr);
+  }
 }
---
+++ frontend/src/app/_types/index.ts
@@ -0,0 +1,13 @@
+export * from "./sort.interface";
+
+export interface Winery {
+  id: number,
+  name: string,
+  description: string,
+  established: number,
+  location: string,
+  region: string,
+  country: string,
+  website: string,
+}
+
