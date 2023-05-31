5824d9850d480bdd2d505bc09ec28fe74bf10f76
vafdaf12 <vafdaf11@gmail.com>
2023-05-31 16:05:44 +0200

    Implement winery component and list

--- frontend/src/app/admin/admin.component.ts
+++ frontend/src/app/admin/admin.component.ts
@@ -1,15 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { MatSort, Sort } from '@angular/material/sort';
+import { WineryService } from '../_services/winery.service';
+import { Winery } from '../_types';
 
 @Component({
   selector: 'app-admin',
   templateUrl: './admin.component.html',
   styleUrls: ['./admin.component.sass']
 })
-export class AdminPage implements OnInit {
+export class AdminPage {
+  wineries: Winery[] = [];
+  @ViewChild(MatSort) sort!: MatSort;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(private wineryService: WineryService) {
+    this.wineryService.getAll()
+    .subscribe(res => this.wineries = res);
   }
-
+  onSort(sortState: any) {
+    sortState = sortState as Sort;
+    alert(sortState.direction);
+  }
 }
--- frontend/src/app/admin/admin.module.ts
+++ frontend/src/app/admin/admin.module.ts
@@ -3,15 +3,31 @@ import { CommonModule } from '@angular/common';
 
 import { AdminRoutingModule } from './admin-routing.module';
 import { AdminPage } from './admin.component';
+import { WineryComponent } from './winery/winery.component';
+
+import { MatCardModule } from "@angular/material/card";
+import { MatButtonModule } from "@angular/material/button";
+import { MatListModule } from "@angular/material/list";
+import { MatIconModule } from "@angular/material/icon";
+import { MatTableModule } from "@angular/material/table";
+import { MatDialogModule } from "@angular/material/dialog";
 
 
 @NgModule({
   declarations: [
-    AdminPage
+    AdminPage,
+    WineryComponent
   ],
   imports: [
     CommonModule,
-    AdminRoutingModule
+    AdminRoutingModule,
+
+    MatCardModule,
+    MatButtonModule,
+    MatListModule,
+    MatIconModule,
+    MatTableModule,
+    MatDialogModule
   ]
 })
 export class AdminModule { }
