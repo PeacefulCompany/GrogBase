1fa229fdd897e41b247ef172bb76398147f5a2f2
u21434809 <u21434809@tuks.co.za>
2023-05-31 16:06:16 +0200

    Added Wineries Scroll List

--- frontend/src/app/home/home.component.ts
+++ frontend/src/app/home/home.component.ts
@@ -7,6 +7,192 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
    Name:string = "Home";
+   TopWine = [
+    {
+      "name": "x",
+      "type": "x",
+      "year": "x",
+      "price": "x",
+      "description": "x"
+    },{
+      "name": "x",
+      "type": "x",
+      "year": "x",
+      "price": "x",
+      "description": "x"
+    },{
+      "name": "x",
+      "type": "x",
+      "year": "x",
+      "price": "x",
+      "description": "x"
+    },{
+      "name": "x",
+      "type": "x",
+      "year": "x",
+      "price": "x",
+      "description": "x"
+    },{
+      "name": "x",
+      "type": "x",
+      "year": "x",
+      "price": "x",
+      "description": "x"
+    },{
+      "name": "x",
+      "type": "x",
+      "year": "x",
+      "price": "x",
+      "description": "x"
+    },{
+      "name": "x",
+      "type": "x",
+      "year": "x",
+      "price": "x",
+      "description": "x"
+    },{
+      "name": "x",
+      "type": "x",
+      "year": "x",
+      "price": "x",
+      "description": "x"
+    },{
+      "name": "x",
+      "type": "x",
+      "year": "x",
+      "price": "x",
+      "description": "x"
+    },{
+      "name": "x",
+      "type": "x",
+      "year": "x",
+      "price": "x",
+      "description": "x"
+    },{
+      "name": "x",
+      "type": "x",
+      "year": "x",
+      "price": "x",
+      "description": "x"
+    },{
+      "name": "x",
+      "type": "x",
+      "year": "x",
+      "price": "x",
+      "description": "x"
+    },{
+      "name": "x",
+      "type": "x",
+      "year": "x",
+      "price": "x",
+      "description": "x"
+    },{
+      "name": "x",
+      "type": "x",
+      "year": "x",
+      "price": "x",
+      "description": "x"
+    },{
+      "name": "x",
+      "type": "x",
+      "year": "x",
+      "price": "x",
+      "description": "x"
+    },{
+      "name": "x",
+      "type": "x",
+      "year": "x",
+      "price": "x",
+      "description": "x"
+    },{
+      "name": "x",
+      "type": "x",
+      "year": "x",
+      "price": "x",
+      "description": "x"
+    },{
+      "name": "x",
+      "type": "x",
+      "year": "x",
+      "price": "x",
+      "description": "x"
+    }
+   ];
+   Localwiener = [
+    {
+      'Name': 'WienerOne',
+      'Established': '2019',
+      'Location': 'Wien',
+      'Country': 'South Africa',
+      'Region': 'Africa',
+      'Website': 'https://www.wiener.com',
+      'Description': 'Wiener is a company that sells hot dogs.'
+    },
+    {
+      'Name': 'WienerTwo',
+      'Established': '2019',
+      'Location': 'Wien',
+      'Country': 'Austria',
+      'Region' : 'Europe',
+      'Website' : 'https://www.wiener.com',
+      'Description' : 'Wiener is a company that sells hot dogs.'
+    },
+    {
+      'Name': 'WienerTwo',
+      'Established': '2019',
+      'Location': 'Wien',
+      'Country': 'Austria',
+      'Region' : 'Europe',
+      'Website' : 'https://www.wiener.com',
+      'Description' : 'Wiener is a company that sells hot dogs.'
+    },
+    {
+      'Name': 'WienerTwo',
+      'Established': '2019',
+      'Location': 'Wien',
+      'Country': 'Austria',
+      'Region' : 'Europe',
+      'Website' : 'https://www.wiener.com',
+      'Description' : 'Wiener is a company that sells hot dogs.'
+    },
+    {
+      'Name': 'WienerTwo',
+      'Established': '2019',
+      'Location': 'Wien',
+      'Country': 'Austria',
+      'Region' : 'Europe',
+      'Website' : 'https://www.wiener.com',
+      'Description' : 'Wiener is a company that sells hot dogs.'
+    },
+    {
+      'Name': 'WienerTwo',
+      'Established': '2019',
+      'Location': 'Wien',
+      'Country': 'Austria',
+      'Region' : 'Europe',
+      'Website' : 'https://www.wiener.com',
+      'Description' : 'Wiener is a company that sells hot dogs.'
+    },
+    {
+      'Name': 'WienerTwo',
+      'Established': '2019',
+      'Location': 'Wien',
+      'Country': 'Austria',
+      'Region' : 'Europe',
+      'Website' : 'https://www.wiener.com',
+      'Description' : 'Wiener is a company that sells hot dogs.'
+    },
+    {
+      'Name': 'WienerTwo',
+      'Established': '2019',
+      'Location': 'Wien',
+      'Country': 'Austria',
+      'Region' : 'Europe',
+      'Website' : 'https://www.wiener.com',
+      'Description' : 'Wiener is a company that sells hot dogs.'
+    }
+   ];
+
   constructor() { 
   
   }
