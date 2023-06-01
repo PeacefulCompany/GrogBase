0586f0430aabc8e5a77d339cdea8c55f5460d208
MichaelHorwitz <91372554+MichaelHorwitz@users.noreply.github.com>
2023-06-01 09:26:32 +0200

    Create api.php

---
+++ backend/api.php
@@ -0,0 +1,19 @@
+<?php
+    //Get connection from database class in config file
+    //Check to see if API key exists
+    //Send to correct endpoint based on type
+
+    //include_once 'wines.php'
+    //include_once 'wineries.php'
+    //include_once 'reviews.php'
+
+    /*We expect something like:
+    {
+    "type":"addWineries",
+    "apikey":"a9198b68355f78830054c31a39916b7f",
+    ...
+    }
+    */
+
+    
+?>
