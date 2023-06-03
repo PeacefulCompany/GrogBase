21c0acdec606fd59f905eebfca995601256cbafb
S3BzA <81089003+S3BzA@users.noreply.github.com>
2023-06-03 14:56:40 +0200

    Re-added modifed config file
    Created credential definitions for Database class.

---
+++ backend/Config.php
@@ -0,0 +1,6 @@
+<?php
+    define('DB_HOST','');//URL or Address of databse host
+    define('DB_USER','');//Username
+    define('DB_PASS','');//Password of database
+    define('DB_NAME','');//Name of the database (Probably GrogBase)
+?>
