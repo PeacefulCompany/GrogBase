f8f972592ec4d454101d4b654c6e34b039ae6288
S3BzA <81089003+S3BzA@users.noreply.github.com>
2023-06-03 16:56:03 +0200

    Added API skeleton
    - created controller and database objects
    - initialised connection var and input json
    - handles exceptions using try-catch

--- backend/API.php
+++ backend/API.php
@@ -10,5 +10,21 @@
     include "Wineries.php";// wineries endpoint
     include "Wines.php";// wines endpoint
 
+    $controller = new Controller();//create controller class
 
+    try
+    {
+        $controller->allowed_methods(["POST"]);// forces user to use POST
+        $controller->assert_params(["api_key", "type"]);// ensures requests contain an API key and request type
+        $db = new Database();// create connection to database
+        
+        $conn = $db->get_connection();// retrieve connection instance
+        $input_json = $controller->get_post_json();// request JSON parsed as associative array
+
+        //handle endpoints according to request type here
+    }
+    catch(exception $e)//handle any caught exceptions
+    {
+        $controller->error($e->getMessage(), $e->getCode());
+    }
 ?>
