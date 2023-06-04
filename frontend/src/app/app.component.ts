e16a3849ed62b2f853d8e57250b7a7b1cb644095
u21434809 <u21434809@tuks.co.za>
2023-06-04 13:13:52 +0200

    Updated Side nav Styling and added hide functionality

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component,ViewChild  } from '@angular/core';
+import { MatSidenav } from '@angular/material/sidenav';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,11 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'frontend';
+  @ViewChild('sidenav') sidenav!: MatSidenav;
+  isSidenavOpened = true;
+
+  toggleSidenav(): void {
+    this.sidenav.toggle();
+    this.isSidenavOpened = !this.isSidenavOpened;
+  }
 }
